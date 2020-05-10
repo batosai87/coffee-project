0fa1987dd4bc15c4c4b5870845e42c8f86bddb99
Eling Meyatmaja <eling.atma@gmail.com>
2020-05-11 06:21:34 +0700

    add page about us minor

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link, graphql} from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from '../components/globals/backgroundsection'
+import Info from "../components/home/info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby` , `application`, `react`]} />
+    <BackgroundSection img={data.img.childImageSharp.fluid} 
+    title="about us" /> 
+    <Info />
+    </Layout>
+)
+
+export const query = graphql`
+{
+  img:file(relativePath:{eq: "latte-back.jpg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
+
+export default AboutPage
